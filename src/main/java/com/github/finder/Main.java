a59044b1a41f87792a7667733d9e8473495b6925
tekitoni <MyHome@MacBookAir.local>
2015-07-04 20:39:03 +0900

    add-main-option、作成しました。
    レビューお願いします。

--- src/main/java/com/github/finder/Main.java
+++ src/main/java/com/github/finder/Main.java
@@ -2,6 +2,14 @@ package com.github.finder;
 
 public class Main{
     public Main(String[] arguments){
+    Args args = new Args();
+        try {
+            CmdLineParser parser = new CmdLineParser(args);
+            parser.parseArgument(arguments);
+        } catch (CmdLineException e) {
+        }
+        return args;
+
     }
 
     public static void main(String[] args){
