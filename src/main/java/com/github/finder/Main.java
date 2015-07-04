ed2335f20aad2b2f2e27df0cc0f95f3cb217c8c0
tekitoni <MyHome@MacBookAir.local>
2015-07-04 23:35:32 +0900

    変更を加えました。
    確認して下さい。
    部分的に修正しています。

--- src/main/java/com/github/finder/Main.java
+++ src/main/java/com/github/finder/Main.java
@@ -2,14 +2,25 @@ package com.github.finder;
 
 public class Main{
     public Main(String[] arguments){
-    Args args = new Args();
+       Args args = parseArguments(arguments);
+
+        Finder finder = new Finder(args);
+        for(String base: args){
+            String[] items = finder.find(base);
+            for(String item: items){
+                System.out.println(item);
+            }
+        }
+    }
+    
+     　private Args parseArguments(String[] arguments){
+        Args args = new Args();
         try {
             CmdLineParser parser = new CmdLineParser(args);
             parser.parseArgument(arguments);
         } catch (CmdLineException e) {
         }
         return args;
-
     }
 
     public static void main(String[] args){
